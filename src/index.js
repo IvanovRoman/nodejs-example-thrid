299a7b84283a8283171baa0a4b09875981789b87
IvanovRoman <werert87@mail.ru>
2019-03-21 13:19:54 +0300

    Defined intermediate function

--- src/index.js
+++ src/index.js
@@ -2,8 +2,38 @@ var http = require("http");
 var fs = require("fs");
 //create a server object:
 http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
+  .createServer((req, res) => {
+    getTitles(res);
   })
-  .listen(8080); //the server object listens on port 8080
+  .listen(8080, "127.0.0.1"); //the server object listens on port 8080
+
+function getTitles(res) {
+  fs.readFile("./src/titles.json", (err, data) => {
+    if (err) {
+      hadError(err, res);
+    } else {
+      getTemplate(JSON.parse(data.toString()), res);
+    }
+  });
+}
+
+function getTemplate(titles, res) {
+  fs.readFile("src/index.html", (err, data) => {
+    if (err) {
+      hadError(err, res);
+    } else {
+      formatHtml(titles, data.toString(), res);
+    }
+  });
+}
+
+function formatHtml(titles, tmpl, res) {
+  const html = tmpl.replace("%", titles.join("<li></li>"));
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(html);
+}
+
+function hadError(err, res) {
+  console.error(err);
+  res.end("Server Error");
+}
