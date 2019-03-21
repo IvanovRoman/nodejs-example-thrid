c43e80ea72008ab08c37f3ffefae0007189fdf19
IvanovRoman <werert87@mail.ru>
2019-03-21 13:22:42 +0300

    Reduced  due to early return

--- src/index.js
+++ src/index.js
@@ -9,21 +9,15 @@ http
 
 function getTitles(res) {
   fs.readFile("./src/titles.json", (err, data) => {
-    if (err) {
-      hadError(err, res);
-    } else {
-      getTemplate(JSON.parse(data.toString()), res);
-    }
+    if (err) return hadError(err, res);
+    getTemplate(JSON.parse(data.toString()), res);
   });
 }
 
 function getTemplate(titles, res) {
   fs.readFile("src/index.html", (err, data) => {
-    if (err) {
-      hadError(err, res);
-    } else {
-      formatHtml(titles, data.toString(), res);
-    }
+    if (err) return hadError(err, res);
+    formatHtml(titles, data.toString(), res);
   });
 }
 
